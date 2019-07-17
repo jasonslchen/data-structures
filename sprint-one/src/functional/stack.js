108f9c547573f62d33ecedc6fdedfae70c9fbae3
Jason Chen <jason.sl.chen@gmail.com>
2019-07-17 12:14:21 -0700

    work on functiona/stack.js

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,27 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  let count = 0;
   // Implement the methods below
   someInstance.push = function(value) {
+    storage.count = value;
+    count++;
+
   };
 
   someInstance.pop = function() {
+    let deleted = storage.count;
+    delete storage.count;
+    count--;
+    return deleted;
+
   };
 
   someInstance.size = function() {
+    if (count < 0) {
+      count = 0;
+    }
+    return count;
   };
 
   return someInstance;
