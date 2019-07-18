72513668c9abd1f33aef8c50e4abddc29aefd1c8
Jason Chen <jason.sl.chen@gmail.com>
2019-07-17 18:28:38 -0700

    commit functional queue.js and stack.js onto solo branch

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -5,14 +5,23 @@ var Queue = function() {
   var storage = {};
 
   // Implement the methods below
+  let max = 0;
+  let min = 0;
 
   someInstance.enqueue = function(value) {
+    storage[max] = value;
+    max++;
   };
 
   someInstance.dequeue = function() {
+    let deletee = storage[min];
+    delete storage[min];
+    min++;
+    return deletee;
   };
 
   someInstance.size = function() {
+    return Math.max(0, max - min);
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,16 +3,41 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  let count = 0;
+  let deleted;
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[count] = value;
+    count++;
+    console.log(storage);
+    console.log(count);
+
   };
 
   someInstance.pop = function() {
+    let index = count - 1;
+    deleted = storage[index];
+    delete storage[index];
+    count--;
+    // console.log(count);
+    return deleted;
+
   };
 
   someInstance.size = function() {
+    if (count < 0) {
+      count = 0;
+    }
+    return count;
   };
 
   return someInstance;
 };
+
+// stack = Stack();
+
+// console.log(stack.push('a'));
+// console.log(stack.push('b'));
+// console.log(stack.push('c'));
+// console.log(stack.pop());
+// console.log(stack.pop());
