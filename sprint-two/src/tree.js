70537e13a84ae55c64cc0941b5b257bf35799633
helloitjase <jason.sl.chen@gmail.com>
2019-07-18 15:34:01 -0700

    tree completed

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,19 +1,33 @@
 var Tree = function(value) {
   var newTree = {};
+
   newTree.value = value;
-
-  // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
-};
+  var treeNode = Tree(value);
+  this.children.push(treeNode);
+ };
 
 treeMethods.contains = function(target) {
+  var result = false;
+  function helper(node){
+    if (node.value === target){
+      result = true;
+    }
+    if (node.children) {
+      node.children.forEach(function(node){
+        helper(node);
+      });
+    }
+  }
+  helper(this);
+  return result;
 };
 
 
