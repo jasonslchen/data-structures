b01aa91d8598d168ed20439b5778101e51833701
helloitjase <jason.sl.chen@gmail.com>
2019-07-18 11:22:35 -0700

    complete link list

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,50 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newNode = Node(value);
+    if (!list.head) {
+      list.head = newNode;
+      list.tail = list.head;
+    }
+    list.tail.next = newNode;
+    list.tail = newNode;
+    return newNode;
   };
 
   list.removeHead = function() {
+    if (!list.head) {
+      return undefined;
+    }
+    var temp = list.head;
+    list.head = list.head.next;
+
+    return temp.value;
   };
 
   list.contains = function(target) {
+    var current = list.head;
+
+    while(current){
+      if (current.value !== target){
+        current = current.next;
+      }else{
+        return true;
+      }
+    }
+    return false;
+
+
+    // let findValue = function(item) {
+    //   let found = false;
+    //   if (item.value === target) {
+    //     found = true;
+    //   } else if (item.next) {
+    //     found = findValue(item.next);
+    //   }
+    //   return found;
+    // }
+
+    // return findValue(current);
   };
 
   return list;
