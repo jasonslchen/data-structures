9815714baccc10f79772c15f6d2bece7d8c4edf1
helloitjase <jason.sl.chen@gmail.com>
2019-07-19 14:02:08 -0700

    finish graph.js

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -28,11 +28,15 @@ Graph.prototype.contains = function(node) {
 Graph.prototype.removeNode = function(node) {
   delete this.nodes[node];
   delete this.edges[node];
+  for(var key in this.edges) {
+    delete this.edges[key][node];
+  }
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
-  if (this.edges[fromNode] && this.edges[toNode]){
+  // debugger;
+  if (this.edges[fromNode][toNode] && this.edges[toNode][fromNode]){
     return true;
   }else{
     return false;
@@ -42,18 +46,26 @@ Graph.prototype.hasEdge = function(fromNode, toNode) {
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
   if (!this.edges[fromNode]){
-    this.edges[fromNode] = fromNode;
+    this.edges[fromNode] = {}
+    this.edges[fromNode][toNode] = toNode;
+  } else {
+    this.edges[fromNode][toNode] = toNode;
   }
+
+  //edge{a: a, b: b, c: c }
   if (!this.edges[toNode]){
-    this.edges[toNode] = toNode;
+    this.edges[toNode] = {}
+    this.edges[toNode][fromNode] = fromNode;
+  } else {
+    this.edges[toNode][fromNode] = fromNode;
   }
 
 };
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
-  delete this.edges[fromNode];
-  delete this.edges[toNode];
+  delete this.edges[fromNode][toNode];
+  delete this.edges[toNode][fromNode];
 };
 
 // Pass in a callback which will be executed on each node of the graph.
