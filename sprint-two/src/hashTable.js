f7d6f6612a41c562e6585a96d5512c35925d270f
helloitjase <jason.sl.chen@gmail.com>
2019-07-19 18:42:05 -0700

    did time complexity for hash.js

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -60,6 +60,8 @@ HashTable.prototype.remove = function(k) {
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ insert : O(n);
+ retrieve : O(n);
+ remove : O(n);
  */
 
-// [      [[k, v], [k, v], [k, v]]              ]
