23054779036e3ca32e317c7080bf2bb41546fa05
helloitjase <jason.sl.chen@gmail.com>
2019-07-20 15:48:21 -0700

    tried to do extra credit hash table

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -3,15 +3,48 @@
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  this.size = 0;
+};
+
+HashTable.prototype.resize = function(size) {
+  var arraylength;
+  if (size > 0.75*this._limit){
+    arraylength = this._limit*2;
+  }else if (size < 0.25*this._limit){
+    arraylength = this._limit/2;
+  }
+
+  this._storage = LimitedArray(arraylength);
+
+  let container = this._storage;
+
+  let currentTuples = [];
+
+
+  container.each(function(bucket) {
+    if (bucket.length > 0) {
+      for (let i = 0; i < bucket.length; i++) {
+        currentTuples.push([bucket[i]]);
+      }
+    }
+  })
+
+  for (let i = 0; i < currentTuples.length; i++) {
+    container.insert(currentTuples[i][0], currentTuples[i][1]);
+  }
+
 
 };
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit); // returns specific index after passing into hashing function
   var container = this._storage;
+
   if (!container.get(index)) {
     let bucket = [[k, v]];
     container.set(index, bucket);
+    this.size++;
+    this.resize(this.size);
   } else {
     let existBucket = container.get(index);
     for (let i = 0; i < existBucket.length; i++) {
@@ -19,14 +52,11 @@ HashTable.prototype.insert = function(k, v) {
         existBucket[i][1] = v;
       } else if (i === existBucket.length - 1) {
         existBucket.push([k, v]);
+        this.size++;
+        this.resize(this.size);
       }
     }
   }
-    // location[index].bucket.push([k, v]);
-
-
-  //this._storage.set(index, bucket);
-  //bucket.set(0, v);
 };
 
 HashTable.prototype.retrieve = function(k) {
@@ -51,6 +81,8 @@ HashTable.prototype.remove = function(k) {
     for (var i = 0; i < position.length; i++){
       if (position[i][0] === k){
         position.splice(i, 1);
+        this.size--;
+        this.resize(this.size);
       }
     }
   }
@@ -65,3 +97,4 @@ HashTable.prototype.remove = function(k) {
  remove : O(n);
  */
 
+// [  [[k, v], [b, c], [a,]]  undefined, undefined,       ]
