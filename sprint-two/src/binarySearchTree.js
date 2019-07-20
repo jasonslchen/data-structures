12f09fdc6f8db5479f105c82843ec3024b8e48e6
helloitjase <jason.sl.chen@gmail.com>
2019-07-19 21:54:46 -0700

    worked on binarysearchtree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,52 @@
 var BinarySearchTree = function(value) {
+
+  this.value = value;
+  this.left = null;
+  this.right = null;
+
 };
 
 
+
+BinarySearchTree.prototype.insert = function(val) {
+//call right and left
+//whichever return true insert value at that children(position);
+  // if(value > this.value) {
+  //   this.right = value;
+  // } else if (value < this.value) {
+  //   this.left = value;
+  // }
+  let current = this.value;
+  let insertion = function(node) {
+
+
+  }
+
+  insertion(this);
+
+}
+
+BinarySearchTree.prototype.contains = function(value) {
+
+  //recurse through tree checking values
+
+}
+
+BinarySearchTree.prototype.depthFirstLog = function(cb) {
+
+  // let each = function(node, call) {
+  //   if (node.value) {
+
+  //   }
+  // }
+}
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+ let tree = new BinarySearchTree(5)
+
+ tree = {value: 5, left: null, right: null}
+
+ tree.insert
+
